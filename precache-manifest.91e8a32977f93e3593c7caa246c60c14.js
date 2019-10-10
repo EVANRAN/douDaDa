c56e34f57292e247df3225ac3ea06d6ec77a2d00
ranshicheng <ranshicheng@163.com>
2019-10-10 18:42:05 +0800

    Updates

---
+++ precache-manifest.91e8a32977f93e3593c7caa246c60c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc1ea0e8bf1a760aaeb7707e15d42c0",
+    "url": "/douDaDa/index.html"
+  },
+  {
+    "revision": "ddc7a291a057bb86a365",
+    "url": "/douDaDa/static/css/2.ce252ba7.chunk.css"
+  },
+  {
+    "revision": "92f1a0b49c700edc6fcf",
+    "url": "/douDaDa/static/css/main.4fb78f1a.chunk.css"
+  },
+  {
+    "revision": "ddc7a291a057bb86a365",
+    "url": "/douDaDa/static/js/2.722dff6c.chunk.js"
+  },
+  {
+    "revision": "92f1a0b49c700edc6fcf",
+    "url": "/douDaDa/static/js/main.5a51f225.chunk.js"
+  },
+  {
+    "revision": "71be2b5300b49c8b0844",
+    "url": "/douDaDa/static/js/runtime-main.91834a1f.js"
+  }
+]);
