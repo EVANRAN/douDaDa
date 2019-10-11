5c3e2281af1032ed15951a5d8c778d7d47d20fc2
ranshicheng <ranshicheng@163.com>
2019-10-11 11:57:38 +0800

    Updates

---
+++ precache-manifest.0e2567757af8db20e8bd954a5d118fa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0413915c29ecaae29cb259e62b8e92",
+    "url": "/douDaDa/index.html"
+  },
+  {
+    "revision": "02b2378f18ce52693acb",
+    "url": "/douDaDa/static/css/2.ce252ba7.chunk.css"
+  },
+  {
+    "revision": "09434f5cb50ac3f05132",
+    "url": "/douDaDa/static/css/main.55429e59.chunk.css"
+  },
+  {
+    "revision": "02b2378f18ce52693acb",
+    "url": "/douDaDa/static/js/2.118efbae.chunk.js"
+  },
+  {
+    "revision": "09434f5cb50ac3f05132",
+    "url": "/douDaDa/static/js/main.19b655b1.chunk.js"
+  },
+  {
+    "revision": "71be2b5300b49c8b0844",
+    "url": "/douDaDa/static/js/runtime-main.91834a1f.js"
+  },
+  {
+    "revision": "44dd3333d0ce70df3163ee28484ec397",
+    "url": "/douDaDa/static/media/yay.44dd3333.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bd5a178feee5744063ea59d60b1f532.js"
+  "/douDaDa/precache-manifest.0e2567757af8db20e8bd954a5d118fa1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/douDaDa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
